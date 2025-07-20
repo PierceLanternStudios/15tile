571c2a364f9987956d575070bf42b04f6cc770d4
pclark04 <pierce.clark@tufts.edu>
2025-07-20 13:56:45 -0400

    implemented createSlice!

--- src/reducers.ts
+++ src/reducers.ts
@@ -1,5 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = { value: 0 };
 
-export default function gameReducer(state = initialState, action) {
-  return state;
+export interface gameState {
+  tilePositions: number[][];
 }
+
+export const gameSlice = createSlice({
+  name: "game",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+  },
+});
+
+export const { increment } = gameSlice.actions;
+
+export default gameSlice.reducer;
