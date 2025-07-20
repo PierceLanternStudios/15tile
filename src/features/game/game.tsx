e4a9d6b8d3628756038d3158861900084492dfc8
pclark04 <pierce.clark@tufts.edu>
2025-07-20 16:13:31 -0400

    first pass at reducer implementation for moving tiles around

--- src/features/game/game.tsx
+++ src/features/game/game.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { selectGrid } from "./gameSlice";
+import { moveUp, moveDown, moveLeft, moveRight, selectGrid } from "./gameSlice";
 import styles from "./game.module.css";
 
 export function GameBoard() {
@@ -9,18 +9,24 @@ export function GameBoard() {
   const grid = useAppSelector(selectGrid);
 
   return (
-    <div className={styles.grid}>
-      {grid.map((row) => (
-        <div className={styles.row}>
-          {row.map((cell) =>
-            cell === 0 ? (
-              <div className={styles.emptyCell}></div>
-            ) : (
-              <div className={styles.cell}>{cell}</div>
-            )
-          )}
-        </div>
-      ))}
+    <div>
+      <div className={styles.grid}>
+        {grid.map((row) => (
+          <div className={styles.row}>
+            {row.map((cell) =>
+              cell === 0 ? (
+                <div className={styles.emptyCell}></div>
+              ) : (
+                <div className={styles.cell}>{cell}</div>
+              )
+            )}
+          </div>
+        ))}
+      </div>
+      <button onClick={() => dispatch(moveUp())}>Up</button>
+      <button onClick={() => dispatch(moveDown())}>Down</button>
+      <button onClick={() => dispatch(moveLeft())}>Left</button>
+      <button onClick={() => dispatch(moveRight())}>Right</button>
     </div>
   );
 }
--- src/features/game/gameSlice.ts
+++ src/features/game/gameSlice.ts
@@ -3,6 +3,7 @@ import { type RootState } from "../../app/store";
 
 export interface gameState {
   grid: number[][];
+  openPos: { row: Number; col: Number };
 }
 
 const initialState: gameState = {
@@ -12,19 +13,73 @@ const initialState: gameState = {
     [8, 9, 10, 11],
     [12, 13, 14, 15],
   ],
+  openPos: { row: 0, col: 0 },
 };
 
 export const gameSlice = createSlice({
   name: "game",
   initialState,
-  reducers: {},
+  reducers: {
+    moveLeft: (state) => {
+      // no-op if empty spot is on right col:
+      if (state.openPos.col === state.grid[0].length - 1) return;
+
+      // otherwise flip flop:
+      const row = Number(state.openPos.row);
+      const col = Number(state.openPos.col);
+      state.grid[row][col] = state.grid[row][col + 1];
+      state.grid[row][col + 1] = 0;
+      state.openPos.col = col + 1;
+      return;
+    },
+
+    moveRight: (state) => {
+      // no-op if empty spot is on left col:
+      if (state.openPos.col === 0) return;
+
+      // otherwise flip flop:
+      const row = Number(state.openPos.row);
+      const col = Number(state.openPos.col);
+      state.grid[row][col] = state.grid[row][col - 1];
+      state.grid[row][col - 1] = 0;
+      state.openPos.col = col - 1;
+      return;
+    },
+
+    moveUp: (state) => {
+      // no-op if empty spot is on bottom row:
+      if (state.openPos.row === state.grid.length - 1) return;
+
+      // otherwise flip flop:
+      const row = Number(state.openPos.row);
+      const col = Number(state.openPos.col);
+      state.grid[row][col] = state.grid[row + 1][col];
+      state.grid[row + 1][col] = 0;
+      state.openPos.row = row + 1;
+      return;
+    },
+
+    moveDown: (state) => {
+      // no-op if empty spot is on right col:
+      if (state.openPos.row === 0) return;
+
+      // otherwise flip flop:
+      const row = Number(state.openPos.row);
+      const col = Number(state.openPos.col);
+      state.grid[row][col] = state.grid[row - 1][col];
+      state.grid[row - 1][col] = 0;
+      state.openPos.col = col - 1;
+      return;
+    },
+  },
 });
 
 //selectors
 export const selectGrid = (state: RootState) => state.game.grid;
+export const selectOpenPos = (state: RootState) => state.game.openPos;
 
 //action generators
-export const {} = gameSlice.actions;
+export const { moveDown, moveLeft, moveRight, moveUp } = gameSlice.actions;
 
 //primary reducer
 export default gameSlice.reducer;
