82a19269df556dbfb4a15b356f071b24ff59b0df
pclark04 <pierce.clark@tufts.edu>
2025-07-20 15:26:00 -0400

    render empty position differently in grid

--- src/features/game/game.tsx
+++ src/features/game/game.tsx
@@ -1,20 +1,25 @@
 import { useState } from "react";
 
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { selectGrid } from "./gameSlice";
+import { selectGrid, selectOpenPos } from "./gameSlice";
 import styles from "./game.module.css";
 
 export function Counter() {
   const dispatch = useAppDispatch();
   const grid = useAppSelector(selectGrid);
+  const openPos = useAppSelector(selectOpenPos);
 
   return (
     <div className={styles.grid}>
-      {grid.map((row) => (
+      {grid.map((row, rowIdx) => (
         <div className={styles.row}>
-          {row.map((cell) => (
-            <div className={styles.cell}>{cell}</div>
-          ))}
+          {row.map((cell, colIdx) =>
+            rowIdx === openPos.row && colIdx === openPos.col ? (
+              <div className={styles.emptyCell}></div>
+            ) : (
+              <div className={styles.cell}>{cell}</div>
+            )
+          )}
         </div>
       ))}
     </div>
--- src/features/game/gameSlice.ts
+++ src/features/game/gameSlice.ts
@@ -3,7 +3,7 @@ import { type RootState } from "../../app/store";
 
 export interface gameState {
   grid: number[][];
-  emptyTile: { row: Number; col: Number };
+  openPos: { row: Number; col: Number };
 }
 
 const initialState: gameState = {
@@ -13,7 +13,7 @@ const initialState: gameState = {
     [8, 9, 10, 11],
     [12, 13, 14, 15],
   ],
-  emptyTile: { row: 0, col: 0 },
+  openPos: { row: 0, col: 0 },
 };
 
 export const gameSlice = createSlice({
@@ -24,6 +24,7 @@ export const gameSlice = createSlice({
 
 //selectors
 export const selectGrid = (state: RootState) => state.game.grid;
+export const selectOpenPos = (state: RootState) => state.game.openPos;
 
 //action generators
 export const {} = gameSlice.actions;
