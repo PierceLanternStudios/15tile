2ce6de614455178bcaca15d53ba81f63913f1809
pclark04 <pierce.clark@tufts.edu>
2025-07-20 15:15:48 -0400

    basic render of grid with CSS

--- src/features/game/game.tsx
+++ src/features/game/game.tsx
@@ -2,9 +2,21 @@ import { useState } from "react";
 
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { selectGrid } from "./gameSlice";
+import styles from "./game.module.css";
 
 export function Counter() {
   const dispatch = useAppDispatch();
+  const grid = useAppSelector(selectGrid);
 
-  return <div></div>;
+  return (
+    <div className={styles.grid}>
+      {grid.map((row) => (
+        <div className={styles.row}>
+          {row.map((cell) => (
+            <div className={styles.cell}>{cell}</div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
