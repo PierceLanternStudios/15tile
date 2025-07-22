ece36848198b9cdc0e2b7a53021de3a4b49824ca
pclark04 <pierce.clark@tufts.edu>
2025-07-22 12:26:53 -0400

    changed reducer logic: moved to returning immutable state, and extracted grid recalculations to a helper function.

--- src/features/game/gameSlice.ts
+++ src/features/game/gameSlice.ts
@@ -22,54 +22,66 @@ export const gameSlice = createSlice({
   reducers: {
     moveLeft: (state) => {
       // no-op if empty spot is on right col:
-      if (state.openPos.col === state.grid[0].length - 1) return;
-
-      // otherwise flip flop:
+      if (state.openPos.col === state.grid[0].length - 1) {
+        console.log("NO-OP!");
+        return state;
+      }
+      // otherwise, flip empty tile and tile in reverse direction:
       const row = Number(state.openPos.row);
       const col = Number(state.openPos.col);
-      state.grid[row][col] = state.grid[row][col + 1];
-      state.grid[row][col + 1] = 0;
-      state.openPos.col = col + 1;
-      return;
+      return {
+        ...state,
+        grid: gridRecalculate(state.grid, row, col, 0, 1),
+        openPos: { row: row, col: col + 1 },
+      };
     },
 
     moveRight: (state) => {
       // no-op if empty spot is on left col:
-      if (state.openPos.col === 0) return;
+      if (state.openPos.col === 0) {
+        console.log("NO-OP!");
+        return state;
+      }
 
-      // otherwise flip flop:
+      // otherwise, flip empty tile and tile in reverse direction:
       const row = Number(state.openPos.row);
       const col = Number(state.openPos.col);
-      state.grid[row][col] = state.grid[row][col - 1];
-      state.grid[row][col - 1] = 0;
-      state.openPos.col = col - 1;
-      return;
+      return {
+        ...state,
+        grid: gridRecalculate(state.grid, row, col, 0, -1),
+        openPos: { row: row, col: col - 1 },
+      };
     },
 
     moveUp: (state) => {
       // no-op if empty spot is on bottom row:
-      if (state.openPos.row === state.grid.length - 1) return;
+      if (state.openPos.row === state.grid.length - 1) return state;
 
-      // otherwise flip flop:
+      // otherwise, flip empty tile and tile in reverse direction:
       const row = Number(state.openPos.row);
       const col = Number(state.openPos.col);
-      state.grid[row][col] = state.grid[row + 1][col];
-      state.grid[row + 1][col] = 0;
-      state.openPos.row = row + 1;
-      return;
+      return {
+        ...state,
+        grid: gridRecalculate(state.grid, row, col, 1, 0),
+        openPos: { row: row + 1, col: col },
+      };
     },
 
     moveDown: (state) => {
       // no-op if empty spot is on right col:
-      if (state.openPos.row === 0) return;
+      if (state.openPos.row === 0) {
+        console.log("NO-OP!");
+        return state;
+      }
 
-      // otherwise flip flop:
+      // otherwise, flip empty tile and tile in reverse direction:
       const row = Number(state.openPos.row);
       const col = Number(state.openPos.col);
-      state.grid[row][col] = state.grid[row - 1][col];
-      state.grid[row - 1][col] = 0;
-      state.openPos.col = col - 1;
-      return;
+      return {
+        ...state,
+        grid: gridRecalculate(state.grid, row, col, -1, 0),
+        openPos: { row: row - 1, col },
+      };
     },
   },
 });
@@ -83,3 +95,52 @@ export const { moveDown, moveLeft, moveRight, moveUp } = gameSlice.actions;
 
 //primary reducer
 export default gameSlice.reducer;
+
+/**
+ * gridRecalculate
+ *
+ * Takes the current state grid and a swap operation (encoded with
+ * row/col offsets) and returns a new array to use as the new grid.
+ * Does so in an immutable way to preserve state.
+ *
+ * @param grid        A reference to the state grid array to create
+ *                    a new version of.
+ * @param openRow     The row index containing the empty tile.
+ * @param openCol     The column index containing the empty tile.
+ * @param rowOffset   The row offset of the swapping tile from the
+ *                    open tile.
+ * @param colOffset   The column offset of the swapping tile from the
+ *                    open tile.
+ */
+function gridRecalculate(
+  grid: number[][],
+  openRow: number,
+  openCol: number,
+  rowOffset: number,
+  colOffset: number
+) {
+  console.log(
+    grid.map((row, rowIdx) => {
+      return row.map((elem, colIdx) => {
+        if (rowIdx === openRow + rowOffset && colIdx === openCol + colOffset) {
+          return 0;
+        }
+        if (rowIdx === openRow && colIdx === openCol) {
+          return grid[rowIdx + rowOffset][colIdx + colOffset];
+        }
+        return grid[rowIdx][colIdx];
+      });
+    })
+  );
+  return grid.map((row, rowIdx) => {
+    return row.map((elem, colIdx) => {
+      if (rowIdx === openRow + rowOffset && colIdx === openCol + colOffset) {
+        return 0;
+      }
+      if (rowIdx === openRow && colIdx === openCol) {
+        return grid[rowIdx + rowOffset][colIdx + colOffset];
+      }
+      return grid[rowIdx][colIdx];
+    });
+  });
+}
